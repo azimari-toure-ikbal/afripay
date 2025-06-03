f3bd89aed20c96d21379ba7d4ca701595687bf31
azimari.toure.ikbal <azimari.toure.etu@esmt.sn>
2025-06-03 15:03:42 +0000

    changes in gitignore

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig([
+  {
+    entry: ['./src/index.ts'],
+    platform: 'neutral',
+    format: ['cjs', 'esm'],
+    dts: true,
+  },
+])
